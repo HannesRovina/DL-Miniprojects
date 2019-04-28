Training function to train multiple times, fixed normalization and other issues with the dataset class

src/convnet.py
@@ -184,14 +184,8 @@ class MaxPoolLayer(nn.Module):
     
 class LinearLayer(nn.Module):
     
-    def __init__(self, inp_shape, out_features, activation='ReLU()', **kwargs):
+    def __init__(self, inp_shape, out_features, activation=None, **kwargs):
         super(LinearLayer, self).__init__()
-        
-        try:
-            assert activation in ACTIV
-        except AssertionError:
-            print(activation + " is not a valid activation function")
-            activation='ReLU()'
             
         if isinstance(inp_shape, int):
             in_features = inp_shape
@@ -206,8 +200,18 @@ class LinearLayer(nn.Module):
         
         block_content = []
         block_content.append(nn.Linear(in_features, out_features))
-        block_content.append(eval('nn.'+activation))
-        self.block = nn.Sequential(*block_content)
+        
+        # Add activation if it was specified
+        if activation is not None:
+            try:
+                assert activation in ACTIV
+            except AssertionError:
+                print(activation + " is not a valid activation function")
+                activation='ReLU()'
+            block_content.append(eval('nn.'+activation))   
+            self.block = nn.Sequential(*block_content)
+        else:
+            self.block = block_content.pop()
         
         block_summary = {'Layer':[],'Input shape':[],'Output shape':[]}
         add_to_summary(block_summary, nn.Linear.__name__, in_features, out_features)
@@ -261,11 +265,24 @@ class BatchNormLayer(nn.Module):
         out = self.block(x)
         return out
     
+def init_weights(module):
+    """
+        Initialize the weights of module.
+        Xavier initialization for Conv and Linear layers
+    """
+    if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+        nn.init.xavier_normal_(module.weight.data)
+    elif isinstance(module, nn.BatchNorm2d):
+        module.reset_parameters()
     
 def train_net(model, device, optimizer, criterion, dataloader, 
                epochs=10, lambda_=1e-3, reg_type=None, save=False):
     
+    # Initialize model weights
+    model.apply(init_weights)  
     model.train()
+    model.to(device)
+    
     avg_epoch_loss_train = []
     avg_epoch_loss_test = []
     avg_epoch_accuracy_train = []
@@ -335,6 +352,67 @@ def train_net(model, device, optimizer, criterion, dataloader,
         	'train_accuracy':avg_epoch_accuracy_train, 
         	'test_loss':avg_epoch_loss_test, 
         	'test_accuracy':avg_epoch_accuracy_test}, model
+            
+def do_train_trials(n_iter, model, device, optim_spec, criterion, dataset, batch_spec,
+               epochs=10, lambda_=1e-3, reg_type=None, save=False):
+    """
+        Assess model over 'n_iter' training trials
+        
+        n_iter          int, how many training runs should be done
+        model           model that is trained
+        optim_spec      dict specifying the optimizer. Should contain a key 'type' 
+                        with a value string corresponding to a optimizer, eg 'SGD'
+                        The other keys specify optimizer-specific parameters,
+                        eg. 'lr', 'momentum'
+        criterion       Loss function
+        dataset         DlDataset instance
+    """
+    
+    trial_perfs = []
+    
+    # Define optimizer
+    try:
+        optim_type = getattr(torch.optim,optim_spec.pop('type'))
+        optim = optim_type(model.parameters(), **optim_spec)
+    except KeyError or AttributeError:
+        optim = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+        print("Invalid optimizer specifications. Defaulting to SGD with lr=0.01, eta=0.5")
+    
+    for i in range(n_iter):
+        
+        # Randomize datasets for each trial and make dataloader
+        dataloader = []
+        dataset.shuffle()
+        for mode in ['train','test']:
+            if mode == 'train':
+                dataset.train()
+            elif mode == 'test':
+                dataset.test()
+            dataloader.append(dataset.return_dataloader(**batch_spec))    
+        
+        # Train
+        performance, _ = train_net(model, device, optim, criterion, dataloader,
+                                    epochs=epochs, lambda_=1e-3, reg_type=None, 
+                                    save=False)
+        trial_perfs.append(performance)
+    
+    perf_as_tensor = {}
+    for i,trial in enumerate(trial_perfs):
+        for key, val in trial.items():
+            assert isinstance(val, list)
+            if i == 0:
+                perf_as_tensor[key] = [torch.Tensor(val)]
+            else:
+                perf_as_tensor[key].append(torch.Tensor(val))
+                
+    overall_perf = {}
+    for key, val in perf_as_tensor.items():
+        total = torch.stack(val, dim=0)
+        overall_perf['avg_'+key] = total.mean(dim=0)
+        overall_perf['std_'+key] =  total.std(dim=0)
+    
+    return ModelPerformanceSummary(model, overall_perf)
+        
 
 def evaluate_net_classes(model, dataset):
     
@@ -369,7 +447,7 @@ class ModelPerformanceSummary:
     """
     def __init__(self, model, perf):
         assert isinstance(model, NumNet)
-        self.model = model
+        self.model = model.name()
         
         if perf is not None:
             self.performance = perf
@@ -387,7 +465,7 @@ class ModelPerformanceSummary:
         try:
             data = self.performance[perf_name]
         except KeyError:
-            print(perf_name + "not evaluated for model " + self.model.name())
+            print(perf_name + "not evaluated for model " + self.model)
             return None
         
         return data

src/data_loading.py
@@ -5,7 +5,7 @@ Created on Wed Apr  3 18:38:49 2019
 @author: silus
 """
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torch.nn import functional as F
 from .dlc_practical_prologue import generate_pair_sets
 
@@ -30,23 +30,9 @@ class DlDataset(Dataset):
         if upsample is not None:
             assert isinstance(upsample, tuple)
             self.upsample = upsample
-    
-        data = generate_pair_sets(N)
-        if self.split == False:
-            self.dataset = data
-        else:
-            (tr_inp, tr_target, tr_classes, te_inp, te_target, te_classes) = data
-            self.left_images = [tr_inp.narrow(1,0,1),tr_classes.narrow(1,0,1).view(-1),tr_target,
-                                te_inp.narrow(1,0,1),te_classes.narrow(1,0,1).view(-1),te_target]
-            self.right_images = [tr_inp.narrow(1,1,1),tr_classes.narrow(1,1,1).view(-1),tr_target,
-                                te_inp.narrow(1,1,1),te_classes.narrow(1,1,1).view(-1),te_target]
-            
-            self.dataset = self.left_images
-            
         
-        self.shape = self.dataset[0].shape
-        self.stats_tr = self.dataset[0].mean(), self.dataset[0].std()
-        self.stats_te = self.dataset[3].mean(), self.dataset[3].std()
+        self.load_random_data_()
+        self.update_data_prop_()
         
     def __len__(self):
         return self.N
@@ -63,7 +49,7 @@ class DlDataset(Dataset):
         elif self.return_set == 'test':
             #print("Returning test data...")
             inp, target, classes = self.dataset[3:]
-            stats = self.stats_tr
+            stats = self.stats_te
         
         if self.normalize:
             inp.sub_(stats[0].div_(stats[1]))
@@ -73,16 +59,48 @@ class DlDataset(Dataset):
         
         return {'input':inp[idx], 'target':target[idx], 'classes':classes[idx]}
     
+    def load_random_data_(self):
+        
+        data = generate_pair_sets(self.N)
+        if not self.split:
+            self.dataset = data
+        else:
+            (tr_inp, tr_target, tr_classes, te_inp, te_target, te_classes) = data
+            self.left_images = [tr_inp.narrow(1,0,1),tr_classes.narrow(1,0,1).view(-1),tr_target,
+                                te_inp.narrow(1,0,1),te_classes.narrow(1,0,1).view(-1),te_target]
+            self.right_images = [tr_inp.narrow(1,1,1),tr_classes.narrow(1,1,1).view(-1),tr_target,
+                                te_inp.narrow(1,1,1),te_classes.narrow(1,1,1).view(-1),te_target]
+            
+            self.dataset = self.left_images
+    
+    def update_data_prop_(self):
+        self.shape = self.dataset[0].shape
+        self.stats_tr = self.dataset[0].mean(), self.dataset[0].std()
+        self.stats_te = self.dataset[3].mean(), self.dataset[3].std()
+        
+    def shuffle(self):
+        """
+            Reload a N random samples
+        """
+        
+        self.load_random_data_()
+        self.update_data_prop_()
+    
     def selectSplittedDataset(self, side):
         if side == 'left':
             self.dataset = self.left_images
         elif side == 'right':
             self.dataset = self.right_images
         
+        self.update_data_prop_()
+        
     def train(self):
         self.return_set = 'train'
     def test(self):
         self.return_set = 'test'
+    
+    def return_dataloader(self, batch_size=4, shuffle=True, num_workers=4):
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
         
     def infere(self, model, idx):
         """
@@ -119,3 +137,4 @@ class DlDataset(Dataset):
             plt.imshow(x[0][1])
 
         return predicted_target, target, classes
+    